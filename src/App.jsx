abdc4da10595184b54b1be02debd4e48beec6f31
Black <Blackstar371@outlook.com>
2025-09-27 08:55:29 -0600

    Inicializacion del repositorio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div className="p-2 h-screen w-screen flex justify-center">
+        <h1 className="text-2xl md:text-5xl text-center hover:text-error transition">
+          Aplicacion PWA <br /> Vite - React - Workbox
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
